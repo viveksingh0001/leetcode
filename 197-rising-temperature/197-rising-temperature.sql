3e1b5769c2de6db4bfd012ad1b7772a678375c9e
viveksingh0001 <39100843+viveksingh0001@users.noreply.github.com>
2022-08-25 09:24:07 +0530

    Time: 634 ms (42.04%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,2 +1,9 @@
 # Write your MySQL query statement below
-select id from weather w1 where temperature > (select temperature from weather w2 where recordDate = DATE_SUB(w1.recordDate, interval 1 day));
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+        AND weather.Temperature > w.Temperature
+;
